ec52e59236ff6bc2dfbb698b36b9330b9b851104
xuchengpu <1550540124@qq.com>
2017-03-21 10:03:33 +0800

    Initial commit

---
+++ app/src/main/java/com/xuchengpu/bilibili/activity/MainActivity.java
@@ -0,0 +1,37 @@
+package com.xuchengpu.bilibili.activity;
+
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.slidingmenu.lib.SlidingMenu;
+import com.slidingmenu.lib.app.SlidingFragmentActivity;
+import com.xuchengpu.bilibili.R;
+import com.xuchengpu.bilibili.utils.DensityUtil;
+
+public class MainActivity extends SlidingFragmentActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        // 去掉窗口标题
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        // 隐藏顶部的状态栏
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        super.onCreate(savedInstanceState);
+        //1.设置主页面
+        setContentView(R.layout.activity_main);
+        //2.设置左侧菜单
+        setBehindContentView(R.layout.leftmenu);
+        //3.设置右侧菜单
+        SlidingMenu slidingMenu = getSlidingMenu();
+
+        //4.设置模式：左侧+主页；左侧+主页+右侧；主页+右侧
+        slidingMenu.setMode(SlidingMenu.LEFT);
+
+        //5.设置滑动的模式：全屏，边缘，不可以滑动
+        slidingMenu.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
+
+        //6.设置主页面占的宽度dip
+        slidingMenu.setBehindOffset(DensityUtil.dip2px(this, 200));
+    }
+}
---
+++ app/src/main/java/com/xuchengpu/bilibili/activity/WelcomeActivity.java
@@ -0,0 +1,41 @@
+package com.xuchengpu.bilibili.activity;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ObjectAnimator;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.LinearLayout;
+
+import com.xuchengpu.bilibili.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @BindView(R.id.activity_welcome)
+    LinearLayout activityWelcome;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+        ButterKnife.bind(this);
+        //设置属性动画
+        ObjectAnimator animalpha=ObjectAnimator.ofFloat(activityWelcome,"alpha",1,1);
+        animalpha.setDuration(1500);
+        //启动动画
+        animalpha.start();
+        //设置监听 跳转到主界面
+        animalpha.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+                startActivity(new Intent(WelcomeActivity.this,MainActivity.class));
+
+            }
+        });
+    }
+}
