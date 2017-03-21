f0e39cc574ddc87d47429046cc4d8ef4458c7115
xuchengpu <1550540124@qq.com>
2017-03-21 11:09:04 +0800

    更改用NavigationView实现左侧侧滑

--- app/src/main/java/com/xuchengpu/bilibili/activity/MainActivity.java
+++ app/src/main/java/com/xuchengpu/bilibili/activity/MainActivity.java
@@ -1,15 +1,23 @@
 package com.xuchengpu.bilibili.activity;
 
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Window;
 import android.view.WindowManager;
 
-import com.slidingmenu.lib.SlidingMenu;
-import com.slidingmenu.lib.app.SlidingFragmentActivity;
 import com.xuchengpu.bilibili.R;
-import com.xuchengpu.bilibili.utils.DensityUtil;
 
-public class MainActivity extends SlidingFragmentActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.navigation_view)
+    NavigationView navigationView;
+    @BindView(R.id.activity_main)
+    DrawerLayout activityMain;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -18,21 +26,15 @@ public class MainActivity extends SlidingFragmentActivity {
         // 隐藏顶部的状态栏
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
         super.onCreate(savedInstanceState);
-        //1.设置主页面
         setContentView(R.layout.activity_main);
-        //2.设置左侧菜单
-        setBehindContentView(R.layout.leftmenu);
-        //3.设置右侧菜单
-        SlidingMenu slidingMenu = getSlidingMenu();
+        ButterKnife.bind(this);
 
-        //4.设置模式：左侧+主页；左侧+主页+右侧；主页+右侧
-        slidingMenu.setMode(SlidingMenu.LEFT);
+        initView();
 
-        //5.设置滑动的模式：全屏，边缘，不可以滑动
-        slidingMenu.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
 
-        //6.设置主页面占的宽度dip
-        slidingMenu.setBehindOffset(DensityUtil.dip2px(this, 200));
+    }
 
+    private void initView() {
+        navigationView.setItemIconTintList(null);
     }
 }
